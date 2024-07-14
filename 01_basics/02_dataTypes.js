af9b5fb99ce7f7ace29321c887812bea6713346a
prernasky <167566630+prernasky@users.noreply.github.com>
2024-07-14 16:55:23 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict";//treat all JS code as newer version
+
+//alert(3+3)//we are using nodejs,not browser
+
+console.log(3
+    +
+     3);//code readability should be high
+
+console.log("Prerna");
+
+let name = "Prerna"
+let age = 19
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power of 53
+// bigint
+// string =>""
+// boolean =>true/false
+// null => standalone value
+// undefined =>
+// symbol => unique 
+
+// object
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
