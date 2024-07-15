f8baaab316bb4ced689f89e39764b9b424d75fe9
prernasky <167566630+prernasky@users.noreply.github.com>
2024-07-15 18:13:02 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true =>1; false =>0
+// null => number; undefined => NaN
+
+let isLoggedIn = "prerna"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
+
+// 1 => true; 0 =>false
+// "" => false
+// "prerna" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
