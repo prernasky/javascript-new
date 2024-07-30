f6608f88097219b47220ea03bd7740cbfd82128c
prernasky <167566630+prernasky@users.noreply.github.com>
2024-07-30 17:10:03 +0000

    datatypes and their comparisons

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,28 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);// different datatypes
+// console.log("02" > 1);
+
+// console.log(null > 0); // false
+// console.log(null == 0); // false    //should avoid because may confuse
+// console.log(null >= 0); // true
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);    //false in every undefined case
+// console.log(undefined < 0);
+
+//  === 
+console.log("2" === 2); //strictly compares along with the datatypes
+
+
+
+
+
+
+
+
